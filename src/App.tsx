9dea7d4297e292b4e8c289524927e1e557d38577
danlee0677 <danlee0677@gmail.com>
2025-05-16 23:26:03 +0900

    Change App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,265 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useDisclosure } from '@mantine/hooks';
+import { Alert, Button, Checkbox, TextInput, Container, Stack, Table, Modal, NumberInput } from '@mantine/core'
+import '@mantine/core/styles.css'
+
+interface Product {
+  price: number;
+  name: string;
+  count: number;
+}
+
+interface Products {
+  category: string;
+  categoryList: Product[];
+}
+
+function FilterableProductTable({ products } : { products: Products[] }) {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <Stack gap="md">
+      <SearchBar 
+        filterText={filterText} 
+        inStockOnly={inStockOnly} 
+        setFilterText={setFilterText} 
+        setInStockOnly={setInStockOnly} />
+      <ProductTable 
+        products={products} 
+        filterText={filterText}
+        inStockOnly={inStockOnly} />
+    </Stack>
+  );
+}
+
+function ProductCategoryRow({ category }: { category: string }) {
+  return (
+    <Table.Tr>
+      <Table.Th>{category}</Table.Th>
+    </Table.Tr>
+  );
+}
+
+function ProductRow({ product }: { product: Product }) {
+  const name = (product.count > 0) ? product.name :
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
+
+  return (
+    <Table.Tr>
+      <Table.Td>{name}</Table.Td>
+      <Table.Td>{`\$${product.price}`}</Table.Td>
+    </Table.Tr>
+  );
+}
+
+function ProductTable({ products, filterText, inStockOnly }: { products: Products[], filterText: string, inStockOnly: boolean }) {
+  const rows: any = [];
+
+  products.forEach((category) => {
+    rows.push(
+      <ProductCategoryRow 
+        category={category["category"]}
+        key={category["category"]}/>
+    )
+    category["categoryList"].forEach((product) => {
+      if (
+        product.name.toLowerCase().indexOf(
+          filterText.toLowerCase()
+        ) === -1
+      ) {
+        return;
+      }
+      if (inStockOnly && !(product.count > 0)) {
+        return;
+      }
+      rows.push(
+        <ProductRow
+          product={product}
+          key={product.name} />
+      );
+    })
+  });
+
+  return (
+    <Table>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>Name</Table.Th>
+          <Table.Th>Price</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>{rows}</Table.Tbody>
+    </Table>
+  );
+}
+
+function SearchBar({
+  filterText,
+  inStockOnly,
+  setFilterText,
+  setInStockOnly
+}: {
+  filterText: string;
+  inStockOnly: boolean;
+  setFilterText: React.Dispatch<React.SetStateAction<string>>;
+  setInStockOnly: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  return (
+    <Stack gap="sm">
+      <TextInput value={filterText} placeholder="Search..."
+      onChange={(event) => setFilterText(event.currentTarget.value)} />
+      <Checkbox label="Only show products in stock" checked={inStockOnly}
+      onChange={(event) => setInStockOnly(event.currentTarget.checked)} />
+    </Stack>
+  );
+}
+
+function handleProductAddButtonClick({
+  productList,
+  setProductList,
+  close,
+  newCategory,
+  newProductName,
+  newProductCount,
+  newProductPrice,
+  setNewCategory,
+  setNewProductName,
+  setNewProductCount,
+  setNewProductPrice,
+  setAlert,
+  setAlertMsg,
+}: {
+  productList: Array<Products>;
+  setProductList: React.Dispatch<React.SetStateAction<Array<Products>>>;
+  close: () => void;
+  newCategory: string;
+  newProductName: string;
+  newProductCount: string | number;
+  newProductPrice: string | number;
+  setNewCategory: React.Dispatch<React.SetStateAction<string>>;
+  setNewProductName: React.Dispatch<React.SetStateAction<string>>
+  setNewProductCount: React.Dispatch<React.SetStateAction<number|string>>
+  setNewProductPrice: React.Dispatch<React.SetStateAction<number|string>>
+  setAlert: React.Dispatch<React.SetStateAction<boolean>>;
+  setAlertMsg: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  if (typeof newProductCount === 'string') {
+    setAlertMsg("No count entered");
+    setAlert(true);
+  } else if (newProductName === "") {
+    setAlertMsg("No product name");
+    setAlert(true);
+  } else if (newCategory === "") {
+    setAlertMsg("No product category");
+    setAlert(true);
+  } else if (typeof newProductPrice === 'string') {
+    setAlertMsg("No price entered")
+    setAlert(true);
+  } else {
+    const found = productList.findIndex((product) => (product["category"] == newCategory));
+    if (found === -1) {
+      setProductList([...productList, {category: newCategory, categoryList: [{price: newProductPrice, count: newProductCount, name: newProductName}]}])
+    } else {
+      setProductList(productList.map((category) => 
+        category["category"] === newCategory ? {
+          ...category, categoryList: [...category["categoryList"], {price: newProductPrice, name: newProductName, count: newProductCount}]
+        } : category
+      ));
+    }
+    setNewCategory("");
+    setNewProductCount("");
+    setNewProductName("");
+    setNewProductPrice("");
+    close();
+  }
+}
+
+function ProductAddButton({
+  productList,
+  setProductList
+}: {
+  productList: Array<Products>;
+  setProductList: React.Dispatch<React.SetStateAction<Array<Products>>>;
+}) {
+  const [opened, {open, close}] = useDisclosure(false);
+  const [productCategory, setProductCategory] = useState("");
+  const [newProductName, setNewProductName] = useState("");
+  const [newProductPrice, setNewProductPrice] = useState<string|number>("");
+  const [productCount, setProductCount] = useState<string|number>('');
+  const [alert, setAlert] = useState(false);
+  const [alertMsg, setAlertMsg] = useState("");
+
+  return (
+    <>
+      <Modal opened={opened} onClose={close} title="Add Product" centered>
+        <Stack gap="md">
+          <TextInput label="Product Category" value={productCategory}
+          onChange={(event)=>setProductCategory(event.currentTarget.value)} />
+
+          <TextInput label="New Product Name" value={newProductName}
+          onChange={(event)=>setNewProductName(event.currentTarget.value)} />
+
+          <NumberInput label="Amount" value={productCount}
+          onChange={setProductCount} />
+
+          <NumberInput label="Price" value={newProductPrice}
+          onChange={setNewProductPrice} />
+
+          {alert ? 
+          <Alert withCloseButton title="Error"
+          onClose={() => setAlert(false)}>{alertMsg}</Alert> : 
+          <></>}
+          
+          <Button size="xs" onClick={() => {
+            handleProductAddButtonClick({productList: productList, 
+              setProductList: setProductList, 
+              close: close, 
+              newCategory: productCategory, 
+              newProductName: newProductName, 
+              newProductCount: productCount, 
+              newProductPrice: newProductPrice, 
+              setNewCategory: setProductCategory,
+              setNewProductCount: setProductCount,
+              setNewProductName: setNewProductName,
+              setNewProductPrice: setNewProductPrice,
+              setAlert: setAlert, 
+              setAlertMsg: setAlertMsg});
+          }}>Done</Button>
+
+        </Stack>
+      </Modal>
+
+      <Button size="xs" onClick={open}>
+        Add Product
+      </Button>
+    </>
+  )
+}
+
+const PRODUCTS: Products[] = 
+  [{category: "Fruits", categoryList: [{price: 1, name: "Apple", count: 1},
+  {price: 1, name: "Dragonfruit", count: 1},
+  {price: 2, name: "Passionfruit", count: 0}]},
+
+  {category: "Vegetables", categoryList: [{price: 2, name: "Spinach", count: 1},
+  {price: 4, name: "Pumpkin", count: 0},
+  {price: 1, name: "Peas", count: 1}]}
+  ];
 
 function App() {
+  const [productList, setProductList] = useState(PRODUCTS);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container size="xs" px="md" mt={40}>
+      <Stack gap="md">
+        <FilterableProductTable products={productList} />
+        <ProductAddButton productList={productList} setProductList={setProductList}/>
+      </Stack>
+    </Container>
   );
 }
 
